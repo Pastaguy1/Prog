b73584439b1d54c23a22887fb0f1840f50fb2645
Pastaguy1 <125645624+Pastaguy1@users.noreply.github.com>
2024-09-11 16:54:48 +0200

    Add files via upload
    Tijdreizen

---
+++ tijdreizen.js
@@ -0,0 +1,8 @@
+const date = new Date(1985, 6, 12, 10, 39, 5, 10);    
+console.log(date);
+const date1 = new Date(1955, 8, 23, 10, 39, 5, 10);    
+console.log(date1);
+const date2 = new Date(2015, 7, 12, 31, 39, 5, 10);    
+console.log(date2);
+const date3 = new Date(1885, 12, 4, 10, 39, 5, 10);    
+console.log(date3);
